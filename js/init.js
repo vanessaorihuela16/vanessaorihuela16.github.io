80771c9368df80cddfcc9c9d44368d3649b7c1d3
vanessaorihuela16 <81536230+vanessaorihuela16@users.noreply.github.com>
2021-04-11 01:31:44 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyC4ZHGdWUpeSVYkoa5IstXZf_2MrLMIb24",
+    authDomain: "bdalum42.firebaseapp.com",
+    projectId: "bdalum42",
+    storageBucket: "bdalum42.appspot.com",
+    messagingSenderId: "34679409053",
+    appId: "1:34679409053:web:8ca4d76475b24d0c090774",
+    measurementId: "G-97X30Q85CH"
+});
