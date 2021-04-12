c95ecdff921e1eb9f921a25350432da59e8e1a95
vanessaorihuela16 <81536230+vanessaorihuela16@users.noreply.github.com>
2021-04-12 00:17:08 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyDOzi7y2Z51E4cPXII9tgn15r5HTjZQuY4",
+    authDomain: "autentica42-v.firebaseapp.com",
+    projectId: "autentica42-v",
+    storageBucket: "autentica42-v.appspot.com",
+    messagingSenderId: "327059903931",
+    appId: "1:327059903931:web:978bfc339593dea6d4a09e",
+    measurementId: "G-ZQJCFJXWSG"
+    });
