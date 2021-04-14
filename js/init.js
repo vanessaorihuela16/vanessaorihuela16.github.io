fba2a7ebe968815d41e6cbf07691e635b0ea92b2
vanessaorihuela16 <81536230+vanessaorihuela16@users.noreply.github.com>
2021-04-13 22:29:59 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,10 @@
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
+    authDomain: "bdfalumnox.firebaseapp.com",
+    projectId: "bdfalumnox",
+    storageBucket: "bdfalumnox.appspot.com",
+    messagingSenderId: "1002536188546",
+    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
+    measurementId: "G-5HHSLWQ24J"
+});
